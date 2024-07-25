853aeff73ac476e30b4eb10dca5e37fbd4a4bac4
Cedric12850 <cedric.deswarte81@gmail.com>
2024-07-25 21:47:33 +0200

    test git push

---
+++ verif_formulaire-main/script_formulaire.js
@@ -0,0 +1,17 @@
+// selecteur des champ de formulaire
+let input = document.querySelectorAll('input, textarea');
+console.log(input);
+
+// selecteur bouton envoyer
+let contactBtn = document.querySelector('#contactBtn')
+    console.log(contactBtn);
+
+// initialisation du tableau de saisie
+let saisie = ['revberg','feazfzf'];
+let stockSaisie = localStorage.getItem('saisie');
+//let stockSaisieText = stockSaisie.split ('');
+
+console.log(saisie);
+contactBtn.addEventListener('click', ()=>{
+    saisie.document.querySelector()
+})
