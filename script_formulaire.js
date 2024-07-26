4d0e5a71bb892344408380dbc7483a37e70b02f8
Cedric12850 <cedric.deswarte81@gmail.com>
2024-07-26 09:40:48 +0200

    on recommence

---
+++ script_formulaire.js
@@ -0,0 +1,13 @@
+// méthode qui précharge le Dom
+document.addEventListener('DOMContentLoaded', function() {
+let champ = document.getElementById('champ');
+let name = document.getElementById('name');
+let prnom = document.getElementById('prenom');
+let pseudo = document.getElementById('pseudo');
+let mail = document.getElementById('mail');
+let checkbox = document.getElementById('checkbox');
+let erreur = document.getElementById('erreur')
+
+
+
+})
