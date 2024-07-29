65c87e6c8fcec3389faa4f825023c6fd5006a8e7
Cedric12850 <cedric.deswarte81@gmail.com>
2024-07-29 12:00:05 +0200

    Formulaire terminé

--- script_formulaire.js
+++ script_formulaire.js
@@ -1,7 +1,6 @@
 // méthode qui précharge le Dom
 document.addEventListener('DOMContentLoaded', function() {
 
-
     // cible les élements input
 let champ = document.getElementById('champ');
 let name = document.getElementById('name');
@@ -9,8 +8,9 @@ let prenom = document.getElementById('prenom');
 let pseudo = document.getElementById('pseudo');
 let mail = document.getElementById('mail');
 let checkbox = document.getElementById('checkbox');
+let checkMessage = document.getElementById('checkMessage');
 let contactBtn = document.getElementById('contactBtn');
-
+let envoiFormulaire = document.getElementById('envoiFormulaire')
 
     // cible les span erreur
 let nameErreur = document.getElementById('nameErreur');
@@ -19,15 +19,18 @@ let pseudoErreur = document.getElementById('pseudoErreur');
 let mailErreur = document.getElementById('mailErreur');
 let checkErreur = document.getElementById('checkErreur');
 
+
 // vérification de la validité du champ nom
 function nomValide() {
     let nomValide = name.value.trim();
     if (nomValide == '') {
         name.style.border = "2px solid red";
         nameErreur.textContent = "Le champ ne doit pas être vide"
+        return false;
     }else{
         name.style.border = "2px solid green";;
         nameErreur.textContent = "";
+        return true;
     }
 }
 name.addEventListener('blur', nomValide);
@@ -39,9 +42,11 @@ function prenomValide() {
     if (prenomValide == '') {
         prenom.style.border = "2px solid red";
         prenomErreur.textContent = "Le champ ne doit pas être vide"
+        return false;
     }else{
         prenom.style.border = "2px solid green";;
         prenomErreur.textContent = "";
+        return true;
     }
 }
 prenom.addEventListener('blur', prenomValide);
@@ -52,10 +57,12 @@ function pseudoValide() {
     let pseudoValide = pseudo.value.trim();
     if (pseudoValide == '') {
         pseudo.style.border = "2px solid red";
-        pseudoErreur.textContent = "Le champ ne doit pas être vide"
+        pseudoErreur.textContent = "Le champ ne doit pas être vide";
+        return false;
     }else{
         pseudo.style.border = "2px solid green";;
         pseudoErreur.textContent = "";
+        return true;
     }
 }
 pseudo.addEventListener('blur', pseudoValide);
@@ -65,25 +72,30 @@ function textValide() {
     let textValide = message.value.trim();
     if (textValide == '') {
         message.style.border = "2px solid red";
-        textErreur.textContent = "Le champ ne doit pas être vide"
+        textErreur.textContent = "Le champ ne doit pas être vide";
+        return false;
     }else{
         message.style.border = "2px solid green";;
         textErreur.textContent = "";
+        return true;
     }
 }
-message.addEventListener('blur', textValide);
+message.addEventListener('blur', textValide); // 'blur' pour le changement d'input
 
 
 
 // vérification de la validité du champ checkbox
-function checkValide() {
-    let checkValide = checkbox.value.trim();
-    if (checkValide == '') {
-        checkbox.style.border = "2px solid red";
-        checkErreur.textContent = "Le champ ne doit pas être vide"
+function checkValide (){
+    let checkValide = document.getElementById(checkErreur)
+    if (checkbox.checked == true){
+        checkMessage.style.color="green";
+        checkErreur.textContent ="";
+        return true;
     }else{
-        checkbox.style.border = "2px solid green";;
-        checkErreur.textContent = "";
+        checkMessage.style.color = "red";
+        checkErreur.style.color ="red";
+        checkErreur.textContent = "Vous devez accepter les termes et conditions";
+        return false;
     }
 }
 checkbox.addEventListener('blur', checkValide);
@@ -93,26 +105,41 @@ checkbox.addEventListener('blur', checkValide);
 // vérification de la validité du champ mail
 function mailValide() {
     let mailValide = mail.value.trim();
-    console.log(mailValide)
+  
     let formMail = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
     if (mailValide == '') {
         mail.style.border = "2px solid red";
         mailErreur.textContent = "Le champ ne doit pas être vide"
-        
+        return false;
     }else{
         if(!formMail.test(mailValide)){
-            console.log(formMail.test(mailValide))  //si le test renvoie false. Le message d'erreur
+            console.log(formMail.test(mailValide))  //si le test renvoie false. Le message d'erreur forma est affiché
             mail.style.border = "2px solid red";
             mailErreur.textContent = "Le format mail n'est pas respecté";
+            return false;
         }else{
         mail.style.border = "2px solid green";;
         mailErreur.textContent = ""; 
+        return true;
         }
     }
 }
 mail.addEventListener('blur', mailValide);
 
+// Vérification du bouton envoyer
+contactBtn.addEventListener ('click',(e)=>{
+    e.preventDefault(); // empèche l'envoi du formulaire
+console.log(e)
 
-
+    if (!nomValide()|| !prenomValide() || !pseudoValide() || !textValide() || !checkValide() || !mailValide()){  // si formulaire false = message d'erreur
+        envoiFormulaire.textContent = "Votre formulaire présente des erreurs et ne peut donc pas être envoyé.";
+        envoiFormulaire.style.color = "red"; 
+    } else{
+        champ.submit();
+        envoiFormulaire.textContent ="Formulaire Envoyé"
+    }
+    
+   
+})
 
 })  // fin de la commande qui vérifie le DOM
